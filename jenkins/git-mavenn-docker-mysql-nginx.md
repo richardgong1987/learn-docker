1f71bf8d1634a8fc093c1cd0284c00b433d77c39
richard <richard123@ex.cc>
2019-05-15 22:56:32 +0800

    a

---
+++ jenkins/git-mavenn-docker-mysql-nginx.md
@@ -0,0 +1,16 @@
+# 实现git+maven+docker+mysql+nginx
+- 目标： git拉代码 -> maven打包 -> docker构建 -> docker执行
+
+## 准备的插件：
+# multiJob Project插件
+# maven
+# bitbucket
+# docker
+
+
+
+
+
+
+
+
--- jenkins/jenkinsondocker.sh
+++ jenkins/jenkinsondocker.sh
@@ -4,3 +4,5 @@ docker pull jenkins/jenkins:lts
 
 
 docker run -d -v /root/jenkins_home:/var/jenkins_home -p 8888:8080 -p 50000:50000 jenkins/jenkins:lts
+
+docker run -d -v /private/jenkins_home:/var/jenkins_home -p 8888:8080 -p 50000:50000 jenkins/jenkins:lts
