2c2b09654fb036f177312d93f1aa98bbfa20d4cf
richardgong <richardgong1987@qq.com>
2019-12-28 14:31:02 +0800

    Update installjdk8-maven-oncentos7.sh

--- docker/installjdk8-maven-oncentos7.sh
+++ docker/installjdk8-maven-oncentos7.sh
@@ -1,47 +1,38 @@
 #!/usr/bin/env bash
 
-## wget http://mirror.bit.edu.cn/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
-## 下载好apache-maven-3.6.1和jdk-8u211-linux-x64.tar.gz上传到服务器
+## wget http://mirror.bit.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+## 下载好apache-maven-3.6.3和jdk-8u231-linux-x64.tar.gz上传到服务器
 
 ## install JDK
 
-tar -xzvf jdk-8u211-linux-x64.tar.gz
+tar -xzvf jdk-8u231-linux-x64.tar.gz
 
 mkdir /usr/lib/jdk
 
-mv jdk1.8.0_211 /usr/lib/jdk
+mv jdk1.8.0_231 /usr/lib/jdk
 
-echo export JAVA_HOME=/usr/lib/jdk/jdk1.8.0_211 >> ~/.bashrc
+echo export JAVA_HOME=/usr/lib/jdk/jdk1.8.0_231 >> ~/.zshrc
 
-echo export JRE_HOME=/usr/lib/jdk/jdk1.8.0_211/jre >> ~/.bashrc
+echo export JRE_HOME=/usr/lib/jdk/jdk1.8.0_231/jre >> ~/.zshrc
 
-. ~/.bashrc
 
-echo export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib >> ~/.bashrc
+
 
 
 
 ## install maven
 
-tar -xzvf apache-maven-3.6.1-bin.tar.gz
+tar -xzvf apache-maven-3.6.3-bin.tar.gz
 # -x 解压
 # -z | --gzip | --ungzip 用gzip对存档压缩或解压
 # -v | --verbose 详细显示处理的文件
 #-f | --file [HOSTNAME:]F 指定存档或设备
 
-echo export M2_HOME=`pwd`/apache-maven-3.6.1 >> ~/.bashrc
+echo export M2_HOME=`pwd`/apache-maven-3.6.3 >> ~/.zshrc
 
-. ~/.bashrc
+echo export PATH='${M2_HOME}'/bin:'${JAVA_HOME}'/bin:$PATH >> ~/.zshrc
 
-echo export PATH=${M2_HOME}/bin:${JAVA_HOME}/bin:$PATH >> ~/.bashrc
+echo export CLASSPATH=.:'${JAVA_HOME}'/lib:'${JRE_HOME}'/lib >> ~/.zshrc
 
-. ~/.bashrc
-
-. ~/.bashrc
-
-. ~/.bashrc
-
-
-
-
+. ~/.zshrc
 
